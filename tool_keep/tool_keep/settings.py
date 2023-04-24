732189ceebdfe8ba9652130c1dff5a5756264808
Рустем <rstmgrv@gmail.com>
2023-04-24 12:18:48 +0300

    Сделали URL

--- tool_keep/tool_keep/settings.py
+++ tool_keep/tool_keep/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'desk.apps.DeskConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
